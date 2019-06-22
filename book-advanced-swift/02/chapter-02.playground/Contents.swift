4cf62dd921ac6e8e01f44af1a22f69a28c62a860
Umair Saeed <umair.saeed@gmail.com>
2019-06-21 20:25:23 -0700

    [Advanced Swift] Chapter 02 - Stateful Closures, Map, Filter, Reduce.

--- book-advanced-swift/02/chapter-02.playground/Contents.swift
+++ book-advanced-swift/02/chapter-02.playground/Contents.swift
@@ -1,11 +1,9 @@
 import UIKit
 
-var str = "Hello, playground"
-
+// Parametrizing Functions
 let array: [Int] = [1, 2, 2, 2, 3, 4, 4]
 
 var result: [[Int]] = array.isEmpty ? [] : [[array[0]]]
-
 for (previous, current) in zip(array, array.dropFirst()) {
   if previous == current {
     result[result.endIndex - 1].append(current)
@@ -34,5 +32,53 @@ extension Array {
 }
 
 let parts = array.split { $0 != $01}
-
 print(parts)
+
+
+// Accumulate
+extension Array {
+
+  func accumulate<Result>(
+    _ initialResult: Result,
+    _ nextPartialResult: (Result, Element) -> Result
+  ) -> [Result] {
+    var running = initialResult
+    return map { next in
+      running = nextPartialResult(running, next)
+      return running
+    }
+  }
+}
+[1, 2, 3, 4].accumulate(0, +) // [1, 3, 6, 10]
+
+
+// Reduce
+let fibs = [0, 1, 1, 2, 3, 5]
+let sum = fibs.reduce(0) { total, num in total + num }
+// OR: let sum = fibs.reduce(0, +)
+print(sum)
+
+
+// Use Reduce to implement map
+
+extension Array {
+  func map2<T>(_ transform: Element -> T) -> [T] {
+    return reduce([]) {
+      $0 + [transform($1)]
+    }
+  }
+}
+
+// Use Reduce to implement filter (O(N))
+
+extension Array {
+  func filter3<T>(_ isIncluded: Element -> Bool) -> [Element] {
+    return reduce(into: []) { (result, element) in
+      if isIncluded(element) {
+        result.append(element)
+      }
+    }
+  }
+}
+
+//
